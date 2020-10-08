75bbaa0806482c7b996b4e8b23b57d90044c3a53
Muskan-08 <49245747+Muskan-08@users.noreply.github.com>
2020-10-08 13:28:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+(function($) {
+
+    var form = $("#signup-form");
+    form.steps({
+        headerTag: "h3",
+        bodyTag: "fieldset",
+        transitionEffect: "fade",
+        labels: {
+            previous : 'Previous',
+            next : 'Next',
+            finish : 'Submit',
+            current : ''
+        },
+        titleTemplate : '<div class="title"><span class="title-text">#title#</span><span class="title-number">#index#</span></div>',
+        onFinished: function (event, currentIndex)
+        {
+            alert('Sumited');
+        }
+    });
+    
+    
+})(jQuery);
+
+
